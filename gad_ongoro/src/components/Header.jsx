9380a396019de2a5d91ec5a2996b57e3652dc9f1
Gad-Ongoro <gadongoro1@gmail.com>
2024-03-06 20:21:14 +0300

    REACT Portfolio

---
+++ gad_ongoro/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<header>
+			<div className="header">
+
+				<div className="dp-name-div">
+					<div className="dp-div">
+						<a href="/">
+							<img src="https://gad-ongoro.github.io/personal-portfolio/DP3.jpg" style={{'width': '110px', 'border-radius': '50%'}} alt='NA'></img>
+						</a>
+					</div>
+				
+					<div className="name-div">
+						<h2> <a href="/">Gad Ongoro </a> </h2>
+					</div>
+				</div>
+
+				{/* <div className="nav-div">
+					<ul>
+						<li> <a href="#"> Home </a> </li>
+						<li> <a href="#about"> About </a> </li>
+						<li> <a href="#expertise"> Expertise </a> </li>
+						<li> <a href="#projects"> Projects </a> </li>
+						<li> <a href="#work"> Work </a> </li>
+						<li> <a href="#contact"> Contact </a> </li>
+					</ul>
+				</div> */}
+
+			</div>
+		</header>
+	)
+};
+
+export default Header;
