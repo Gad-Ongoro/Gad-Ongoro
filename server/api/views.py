63ecbbc35b813f4115d436b55cfd693dee932b44
Gad-Ongoro <gadongoro1@gmail.com>
2024-06-05 03:52:25 +0300

    fix

--- server/api/views.py
+++ server/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, views, status, response
+from rest_framework import generics, views, status, response, permissions
+from rest_framework.decorators import permission_classes
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
@@ -17,11 +18,12 @@ class Contact_DetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.ContactSerializer
 
 class SendEmailView(views.APIView):
+    @permission_classes((permissions.AllowAny,))
     def post(self, request, *args, **kwargs):
         serializer = serializers.ContactSerializer(data=request.data)
         if serializer.is_valid():
             name = serializer.validated_data['name']
-            email = serializer.validated_data['email']
+            contact_email = serializer.validated_data['email']
             message = serializer.validated_data['message']
             
             # Send email
@@ -29,19 +31,18 @@ class SendEmailView(views.APIView):
             #     f"Contact form submission from {name}",
             #     message,
             #     email,
-            #     ['wavatar245@fresec.com'],
+            #     ['gadongoro1@gmail.com'],
             #     fail_silently=False,
             # )
 
-            context = {'user': name}
             subject = f'New Contact Message from {name}'
-            from_email = settings.DEFAULT_FROM_EMAIL
+            from_email = contact_email
             to_email = 'gadongoro1@gmail.com'
 
             # Render the HTML template to a string
             html_content = render_to_string('contact_email.html', {
                 'name': name,
-                'email': email,
+                'email': contact_email,
                 'message': message,
             })
             text_content = 'This is an important message.'  # Fallback text content
@@ -53,7 +54,7 @@ class SendEmailView(views.APIView):
             # Send the email
             email.send()
 
-            models.Contact.objects.create(name=name, email=email, message=message)
+            models.Contact.objects.create(name=name, email=from_email, message=message)
 
             return response.Response({'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
