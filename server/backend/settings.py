857de8ae2a130505bb994dfc41842cf1db106eb8
Gad-Ongoro <gadongoro1@gmail.com>
2024-06-05 03:44:57 +0300

    Added PostgreSQL db_url

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     }
 }
 
-# DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
